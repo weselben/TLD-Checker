115b7d7cdec08f74d563480a4155202964cd9eb6
weselben <50115212+weselben@users.noreply.github.com>
2021-03-07 21:57:12 +0100

    Adding Footer
    with Imprint and Privacy Policy

---
+++ imprint.php
@@ -0,0 +1,42 @@
+<?php ?>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="assets/style/main.css" rel="stylesheet">
+    <link rel="icon" type="image/svg+xml" href="assets/imgs/favicon.svg" sizes="any">
+    <title>Legal Disclosure | TLD-Checker</title>
+</head>
+<body>
+<div class="container">
+    <div class="rows">
+        <h1>TLD-Checker - Legal Disclosure</h1>
+        Information in accordance with Section 5 TMG
+        <br><br>
+        <h2>Contact Information</h2>
+        E-Mail: <a href="mailto:bengottwaldi04+tld-checker@gmail.com">bengottwaldi04+tld-checker@gmail.com</a><br>Internet address: <a href="https://tld-checker.net/" target="_blank">https://tld-checker.net/</a><br><br>
+        <h2>Disclaimer</h2>
+        Accountability for content<br>
+        The contents of our pages have been created with the utmost care. However, we cannot guarantee the contents'
+        accuracy, completeness or topicality. According to statutory provisions, we are furthermore responsible for
+        our own content on these web pages. In this matter, please note that we are not obliged to monitor
+        the transmitted or saved information of third parties, or investigate circumstances pointing to illegal activity.
+        Our obligations to remove or block the use of information under generally applicable laws remain unaffected by this as per
+        §§ 8 to 10 of the Telemedia Act (TMG).
+
+        <br><br>Accountability for links<br>
+        Responsibility for the content of
+        external links (to web pages of third parties) lies solely with the operators of the linked pages. No violations were
+        evident to us at the time of linking. Should any legal infringement become known to us, we will remove the respective
+        link immediately.<br><br>Copyright<br> Our web pages and their contents are subject to German copyright law. Unless
+        expressly permitted by law, every form of utilizing, reproducing or processing
+        works subject to copyright protection on our web pages requires the prior consent of the respective owner of the rights.
+        Individual reproductions of a work are only allowed for private use.
+        The materials from these pages are copyrighted and any unauthorized use may violate copyright laws.
+
+        <br><br>
+    </div>
+</div>
+</body>
+<?php include "footer.php"; ?>
+</html>
--- index.php
+++ index.php
@@ -1693,45 +1693,7 @@ if (isset($_POST['submit'])) {
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>';
-} ?>
-    <footer class="page-footer" style="background-color: #3a3a3a;">
-        <div class="container">
-            <div class="row">
-                <div class="col l6 s12">
-                    <h5 class="white-text"><a href="https://tld-checker.net">TLD-Checker.net</a></h5>
-                    <p class="grey-text text-lighten-4">Hey, for the functionality of this website I use JavaScript, so If you have a JavaScript blocker don't complain.</p>
-                </div>
-                <div class="col l4 offset-l2 s12">
-                    <h5 class="white-text">Links</h5>
-                    <ul>
-                        <li><a class="grey-text text-lighten-3" href="https://paypal.me/bengotti">Donate</a></li>
-                        <li><a class="grey-text text-lighten-3" href="https://github.com/weselben/TLD-Checker">GitHub</a></li>
-                        <li><a class="grey-text text-lighten-3" href="https://github.com/weselben/TLD-Checker/issues">Issues</a></li>
-                        <li><a class="grey-text text-lighten-3" href="assets/imgs/cs.jpg">Discord</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright">
-            <div class="container">
-                <?php echo "Copyright &copy; 2020-" . date("Y") . " - weselben"; ?>
-                <div class="grey-text text-lighten-4 right" >
-                        <?php
-                        if (!isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-                            $client_ip = $_SERVER['REMOTE_ADDR'];
-                        } else {
-                            $client_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-                        }
-                        if ($client_ip == "127.0.0.1") {
-                            $city = "localhost";
-                            echo '<a href="https://www.google.com/maps/search/localhost">localhost</a>' . " - " . $client_ip;
-                        } else {
-                            $city = json_decode(file_get_contents("http://ipinfo.io/{$client_ip}/json"));
-                            echo '<a href="https://www.google.com/maps/search/' . $city->city . '">' . $city->city . '</a>' . " - " . $client_ip;
-                        }
-                    ?>
-                </div>
-            </div>
-        </div>
-    </footer>
+}
+include "footer.php";
+?>
 </html>
