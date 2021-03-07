c97154660c3f9f30adfdf3ba894cbfc2bfea3faf
weselben <50115212+weselben@users.noreply.github.com>
2021-03-07 23:50:22 +0100

    Change Title

--- index.php
+++ index.php
@@ -1673,7 +1673,7 @@ if (isset($_POST['submit'])) {
     }
 } else {
     echo '<head>
-    <title>TLD Checker</title>
+    <title>TLD-Checker</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="assets/style/main.css" rel="stylesheet">
