0d41b4565ce443bc7373f587151b15d8be023273
Ben <50115212+weselben@users.noreply.github.com>
2022-02-18 12:24:50 +0100

    Adding one Adsense ad

--- de/index.php
+++ de/index.php
@@ -32,9 +32,7 @@
     }
     ?>
     <!--Google Autoads-->
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6657616209109422"
-     crossorigin="anonymous"></script>
-    
+    <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
 </head>
 <div class="navbar-fixed">
     <nav >
@@ -76,7 +74,14 @@
         }</style>
 </noscript>
 <div class="row">
-
+    <amp-ad width="100vw" height="320"
+     type="adsense"
+     data-ad-client="ca-pub-6657616209109422"
+     data-ad-slot="1527207650"
+     data-auto-format="rspv"
+     data-full-width="">
+      <div overflow=""></div>
+    </amp-ad>
     <?php
     if (isset($_POST['submit'])) {
         $domainname = htmlspecialchars($_POST['domainname'], ENT_QUOTES, 'UTF-8');
